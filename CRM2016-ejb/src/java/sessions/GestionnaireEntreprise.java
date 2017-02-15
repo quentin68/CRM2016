55a95d498801cc52b05d06aa68e032e3f26fa2f6
Quentin Bauer <qbauer68@gmail.com>
2017-02-15 13:44:00 +0100

    Init Projet : Création d'une entreprise/ Modif à compléter

---
+++ CRM2016-ejb/src/java/sessions/GestionnaireEntreprise.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sessions;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author mbuffa
+ */
+@Stateless
+@LocalBean
+public class GestionnaireEntreprise {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    // Creer une entreprise
+    
+    // rechercher une entreprise
+    
+    // renvoyer toutes les entreprises
+    
+}
